bc66c327ce2134a3a8eacda208e5282f2ed8b4a7
dariuspetrik <>
2024-04-24 20:21:32 +0200

    added comments

--- src/test/java/tests/UI/LoginTest.java
+++ src/test/java/tests/UI/LoginTest.java
@@ -9,6 +9,10 @@ import utils.Constants;
 
 public class LoginTest extends UiBaseTest {
 
+  /**
+   * This test verifies successful login for standard user
+   * It is important to know that user can log in with correct credentials
+   */
   @Test
   public void standardUserLogin() {
     page.navigate(Constants.BASEURL);
@@ -16,6 +20,11 @@ public class LoginTest extends UiBaseTest {
     Assert.assertTrue(inventoryPage.getCurrentURL().contains("/inventory.html"));
   }
 
+  /**
+   * This test verifies that user can not log in with empty credentials
+   * It is important to know that system can handle wrong - empty credentials
+   *
+   */
   @Test(dataProviderClass = DataProvider.class, dataProvider = "emptyCredentials")
   public void invalidLogin(String username, String password, String errorMessage) {
     page.navigate(Constants.BASEURL);
@@ -23,6 +32,10 @@ public class LoginTest extends UiBaseTest {
     Assert.assertEquals(loginPage.getInputFieldsErrorMessage(), errorMessage);
   }
 
+  /**
+   * This test verifies that user can not use credentials for locked account
+   * It is important to let the user know about the locked account
+   **/
   @Test
   public void lockedOutUserLogin() {
     page.navigate(Constants.BASEURL);
@@ -30,6 +43,10 @@ public class LoginTest extends UiBaseTest {
     Assert.assertEquals(loginPage.getInputFieldsErrorMessage(), Constants.LOCKED_OUT_USER_ERROR);
   }
 
+  /**
+   * This test verifies how quickly user can log in
+   * It is important to know the system performance (page loading)
+   */
   @Test
   public void performanceGlitchUserLogin() {
     page.navigate(Constants.BASEURL);
